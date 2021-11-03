6ed9ef3294070fd1abf0d096722f1e6ed87b921d
Katelyn Drew <>
2021-11-03 16:11:36 +0000

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("Enter a name");
+    String name= new
   }
 }
