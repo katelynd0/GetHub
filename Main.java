661f29524d3a29068601ce42ba5d356c7b551188
Katelyn Drew <>
2021-11-03 16:12:09 +0000

    new print

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Enter a name");
-    String name= new
+    System.out.print("Enter");
   }
 }
